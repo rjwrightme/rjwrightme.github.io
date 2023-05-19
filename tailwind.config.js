ce151b3f1402eeca10e9c65f98b8938f28125b56
RJ Wright <rj@rjwright.me>
2023-05-19 13:46:54 +1000

    Wipe the old portfolio repo for a new one using Next.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: {
-    content: ["./src/**/*.jsx", "./src/**/*.js"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
   },
   plugins: [],
 };
