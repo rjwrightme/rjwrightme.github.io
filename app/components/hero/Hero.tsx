0a4ca03301e9892b41c5312cd9c1d2093353417d
RJ Wright <rj@rjwright.me>
2023-05-22 18:05:27 +1000

    Add basic styling to hero component.

--- app/components/hero/Hero.tsx
+++ app/components/hero/Hero.tsx
@@ -10,10 +10,10 @@ const Hero = () => {
         <div className="flex flex-col md:flex-row justify-around items-center">
           <div className="flex justify-center items-center">
             <CurlyBrace />
-            <h1 className="dark:text-white text-4xl font-bold text-center ">
+            <h1 className="dark:text-white text-4xl font-bold text-center leading-6">
               <span className="font-mono text-sage-100">Developer</span>
               <br />
-              <span className="font-medium text-lg">by</span>
+              <span className="font-medium text-lg leading-10">by</span>
               <br />
               Design
             </h1>
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "./components/hero/Hero";
 
 export default function Home() {
   return (
-    <div className="dark:bg-sage-600">
+    <div className="bg-gradient-to-t dark:from-sage-400 dark:to-sage-600 dark:via-sage-600 via-80%">
       <Hero />
     </div>
   );
